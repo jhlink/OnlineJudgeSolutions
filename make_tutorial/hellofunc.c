22d2f97639aba803721e0a53054acd79a28ac9ba
jhlink <jhlink@users.noreply.github.com>
2020-01-27 16:37:51 -0500

    style: Adjust spacing

--- make_tutorial/hellofunc.c
+++ make_tutorial/hellofunc.c
@@ -1,9 +1,11 @@
 #include <stdio.h>
 #include <hellomake.h>
 
-void myPrintHelloMake(void) {
+void
+myPrintHelloMake(void)
+{
 
-    printf("Hello makefiles!\n");
+  printf("Hello makefiles!\n");
 
-      return;
+  return;
 }
