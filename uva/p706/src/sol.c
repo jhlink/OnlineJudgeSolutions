0a039783edf6a670791b8ffecc4411c4cc8c06c3
jhlink <jhlink@users.noreply.github.com>
2020-02-25 16:05:01 -0500

    feat: Print lcd formatted digits w/o size scale

--- uva/p706/src/sol.c
+++ uva/p706/src/sol.c
@@ -3,8 +3,7 @@
 #include <stdlib.h> /* alloc funcs */
 #include <string.h> /* memset*/
 #include <stdint.h> /* (un)signed ints and limits */
-
-#define NDEBUG
+#include <stdio.h>  /* putchar */
 
 uint32_t min_uint(uint32_t a, uint32_t b);
 uint32_t max_uint(uint32_t a, uint32_t b);
@@ -36,6 +35,34 @@ static const char DIGIT_MATRIX[NUM_OF_DIGITS][LCD_MATRIX_DIMEN] = {
   /* 9 */ '-', '|', '|', '_', ' ', '|', '-'
 };
 
+void print_digits() {
+  int i, j;
+  int digit = 2;
+
+  for ( digit = 0; digit < NUM_OF_DIGITS; ++digit ) {
+    for ( i = 0; i < LCD_MATRIX_DIMEN; ++i ) {
+      if ( i % 3 == 0 ) {
+        putchar(' ');
+        putchar(DIGIT_MATRIX[digit][i]);
+        putchar(' ');
+        putchar('\n');
+      } else if ( 0 < i && i < 2) {
+        putchar(DIGIT_MATRIX[digit][i]);
+        putchar(' ');
+        putchar(DIGIT_MATRIX[digit][i + 1]);
+        putchar('\n');
+      } else if ( 3 < i && i < 5) {
+        putchar(DIGIT_MATRIX[digit][i]);
+        putchar(' ');
+        putchar(DIGIT_MATRIX[digit][i + 1]);
+        putchar('\n');
+      }
+    }
+
+    putchar('\n');
+  }
+}
+
 void
 process_input()
 {
@@ -46,6 +73,7 @@ int
 main()
 {
   process_input();
+  print_digits();
   return (0);
 }
 
